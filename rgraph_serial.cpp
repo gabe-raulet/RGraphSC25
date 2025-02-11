02a9a5e1d0d7d6dbbd9e1334b112cbc587555b81
Gabriel Raulet <gabe.h.raulet@gmail.com>
2025-02-11 15:21:28 -0800

    results.json

--- rgraph_serial.cpp
+++ rgraph_serial.cpp
@@ -25,24 +25,39 @@ using PointVector = PointTraits::PointVector;
 
 PointVector points;
 size_t num_points;
+json results;
+int nthreads = 1;
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    #pragma omp parallel
+    nthreads = omp_get_num_threads();
+
+    if (argc != 3)
     {
-        fprintf(stderr, "usage: %s <points>\n", argv[0]);
+        fprintf(stderr, "usage: %s <points> <results>\n", argv[0]);
         return 1;
     }
 
-    const char *fname = argv[1];
+    const char *points_fname = argv[1];
+    const char *results_fname = argv[2];
 
     double t;
 
     t = -omp_get_wtime();
-    PointTraits::read_fvecs(points, fname); num_points = points.size();
+    PointTraits::read_fvecs(points, points_fname); num_points = points.size();
     t += omp_get_wtime();
 
-    fmt::print("[time={:.3f}] read {} points from file '{}'\n", t, num_points, fname);
+    fmt::print("[time={:.3f}] read {} points from file '{}'\n", t, num_points, points_fname);
+
+    results["dataset"] = points_fname;
+    results["num_points"] = num_points;
+    results["dim"] = DIM_SIZE;
+    results["num_threads"] = nthreads;
+
+    std::ofstream f(results_fname);
+    f << std::setw(4) << results << std::endl;
+    f.close();
 
     return 0;
 }
