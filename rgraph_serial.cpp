37fc008fa9951ecf447990488df6fccdf995eb3a
Gabriel Raulet <gabe.h.raulet@gmail.com>
2025-02-11 15:41:58 -0800

    build greedy net

--- rgraph_serial.cpp
+++ rgraph_serial.cpp
@@ -8,6 +8,8 @@ using json = nlohmann::json;
 
 #include "fmt/core.h"
 #include "points.h"
+#include <unordered_set>
+#include <unordered_map>
 #include <iostream>
 #include <vector>
 #include <limits>
@@ -31,17 +33,43 @@ using PointVector = std::vector<Point>;
 using IndexVector = std::vector<Index>;
 using RealVector = std::vector<Real>;
 
-Distance distance;
+using IndexMap = std::unordered_map<Index, Index>;
+using IndexSet = std::unordered_set<Index>;
+using IndexVectorMap = std::unordered_map<Index, IndexVector>;
 
 PointVector points;
 Index num_points;
-Index num_sites;
-Real epsilon;
+Index num_sites; /* number of Voronoi sites */
+Real epsilon; /* graph radius */
 json results;
 int nthreads = 1;
 
 Real covering_factor = 2.0;
 Index leaf_size = 10;
+
+Distance distance;
+
+void build_greedy_net(); /* build Voronoi diagram */
+void build_replication_tree(); /* build replication tree on Voronoi sites */
+//void compute_ghost_points(); /* compute ghost points for each Voronoi cell */
+//void build_ghost_trees(); /* build cover trees on Voronoi cells plus ghost points */
+//void build_epsilon_graph(); /* build epsilon graph */
+
+Index range_query(IndexVector& neighbors, Point query, Real radius); /* query replication tree for intersecting Voronoi sites */
+
+IndexVector net; /* indices of Voronoi sites */
+IndexVector cells; /* Voronoi cell site for each point */
+RealVector dists; /* distance of each point to its Voronoi site */
+Real net_sep; /* minimum separation of net sites */
+
+IndexMap repids; /* maps Voronoi sites to their slot */
+std::vector<IndexVectorMap> reptree; /* replication tree */
+Index minlevel, maxlevel, root; /* minimum/maximum level of replication tree, and root point id */
+
+//TreeMap trees; /* maps Voronoi sites to their "ghost" trees */
+//IndexVectorMap treeids, cellids; /* maps net sites to point+ghost_point sets (treeids) and just point sets (cellids) */
+
+//IndexVectorVector graph; /* epsilon graph */
 
 int main(int argc, char *argv[])
 {
@@ -74,9 +102,74 @@ int main(int argc, char *argv[])
     results["num_sites"] = num_sites;
     results["epsilon"] = epsilon;
 
+    build_greedy_net();
+
     std::ofstream f(results_fname);
     f << std::setw(4) << results << std::endl;
     f.close();
 
     return 0;
+}
+
+void build_greedy_net()
+{
+    json my_results;
+
+    Index n = num_points;
+    Index m = num_sites;
+
+    double t;
+
+    t = -omp_get_wtime();
+
+    net.reserve(m);
+    net.push_back(0);
+    cells.resize(n);
+    dists.resize(n);
+    net_sep = -1;
+
+    Index farthest;
+
+    for (Index p = 0; p < n; ++p)
+    {
+        dists[p] = distance(points[net.back()], points[p]);
+
+        if (dists[p] > net_sep)
+        {
+            farthest = p;
+            net_sep = dists[p];
+        }
+    }
+
+    for (Index i = 1; i < m; ++i)
+    {
+        net.push_back(farthest);
+        net_sep = -1;
+
+        for (Index p = 0; p < n; ++p)
+        {
+            Real d = distance(points[net.back()], points[p]);
+
+            if (d < dists[p])
+            {
+                cells[p] = net.back();
+                dists[p] = d;
+            }
+
+            if (dists[p] > net_sep)
+            {
+                farthest = p;
+                net_sep = dists[p];
+            }
+        }
+    }
+
+    t += omp_get_wtime();
+
+    fmt::print("[time={:.3f}] built r-net Voronoi diagram [sep={:.3f},num_sites={}]\n", t, net_sep, m);
+
+    my_results["net_sep"] = net_sep;
+    my_results["time"] = t;
+
+    results["build_greedy_net"] = my_results;
 }
