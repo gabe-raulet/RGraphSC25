438911aba1f1618c8152d48a80f94c8fac4d48ba
Gabriel Raulet <gabe.h.raulet@gmail.com>
2025-02-12 16:50:59 -0800

    add test

--- test.sh
+++ test.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-mpic++ -o rgraph_corel -DVERIFY -DDIM_SIZE=32 -std=c++20 -fopenmp -O2 -I./ rgraph.cpp
-./rgraph_corel datasets/izbicki15/small/corel.fvecs 128 0.0515 results.json
+mpic++ -o rgraph_corel -DDIM_SIZE=32 -std=c++20 -fopenmp -O2 -I./ -I./include rgraph.cpp
+mpic++ -o rgraph_mpi_corel -DDIM_SIZE=32 -std=c++20 -fopenmp -O2 -I./ -I./include rgraph_mpi.cpp
+
+export OMP_NUM_THREADS=12
+
+DATASET=datasets/izbicki15/small/corel.fvecs
+
+rm -rf results
+mkdir -p results
+
+for EPSILON in 0.05 0.10
+    do
+    for CELL_COUNT in 8 16 32 64
+        do
+        ./rgraph_corel -o results/serial.r${EPSILON}.m${CELL_COUNT}.json $DATASET $CELL_COUNT $EPSILON
+        for PROC_COUNT in 1 2 4 8
+            do
+            mpirun -np $PROC_COUNT ./rgraph_mpi_corel -o results/mpi.r${EPSILON}.m${CELL_COUNT}.p${PROC_COUNT}.json $DATASET $CELL_COUNT $EPSILON
+        done
+    done
+done
